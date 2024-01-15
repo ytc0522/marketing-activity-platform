61d0cb67f71d327c878ddafe7c9803abc9963fd3
yaotianchi <yaotianchi@longcom.com>
2024-01-15 16:16:55 +0800

    初始化秒杀项目结构

---
+++ activity-seckill/seckill-domain/src/main/java/org/example/marketing/activity/seckill/domain/model/SeckillGoods.java
@@ -0,0 +1,14 @@
+package org.example.marketing.activity.seckill.domain.model;
+
+import java.io.Serializable;
+
+public class SeckillGoods implements Serializable {
+
+    private static final long serialVersionUID = 6222640566752960875L;
+
+    private Long id;
+
+    private String goodsName;
+
+
+}
