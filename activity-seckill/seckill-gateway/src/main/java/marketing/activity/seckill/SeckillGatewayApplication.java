878dab5a483d560183eeaade6c9b07f1867470cd
yaotianchi <yaotianchi@longcom.com>
2024-01-19 22:12:38 +0800

    增加网关项目

---
+++ activity-seckill/seckill-gateway/src/main/java/marketing/activity/seckill/SeckillGatewayApplication.java
@@ -0,0 +1,13 @@
+package marketing.activity.seckill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeckillGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeckillGatewayApplication.class);
+    }
+
+}
---
+++ marketing-activity/activity-gateway/src/main/java/marketing/activity/gateway/ActivityGatewayApplication.java
@@ -0,0 +1,12 @@
+package marketing.activity.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivityGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivityGatewayApplication.class);
+    }
+}
